c35d3a3f2ec78b4ccf1baf3dc8d35dcb6e384cd3
nonspacial <abstrktrobbie@gmail.com>
2014-07-27 21:24:41 +0100

    fixed Djs page
    defined the while loops better

--- pages/djs.php
+++ pages/djs.php
@@ -5,9 +5,9 @@ include("setup/connection.php");
     <div id="djList" class="col-md-3">
         <div class="list-group" style="border-bottom:1px solid #DCDCDC;border-radius:0;">
 		<?php	
-			$list=$dbc->query("SELECT * FROM dj_profiles ORDER BY dj_name");
+			$djlist=$dbc->query("SELECT * FROM dj_profiles ORDER BY dj_name");
             #Get Page Content Query
-            while($list=mysqli_fetch_assoc($list)) {?>
+            while($list=mysqli_fetch_assoc($djlist)) {?>
              <div id="profile_<?php echo $list['id']; ?>"  style="background:none;padding-bottom:4%;border:0px;"
              class="list-group-item<?php selected($list['id'], $_GET['id'], ' active');?>">
              <a href="Djs?id=<?php echo $list['id'];?>" ><span><h4 class="list-group-item-heading" style="color:#DCDCDC;" ><?php echo $list['dj_name'];?></h4></span></a>
@@ -17,9 +17,9 @@ include("setup/connection.php");
     </div>
     <div id="djProfile" class="col-md-9 panel panel-default" style="background:none;">
        	<?php	$opened=$dbc->query("SELECT * FROM dj_profiles WHERE id=$_GET[id]");
-				$list=mysqli_fetch_assoc($opened);
-			 	if ($list) { ?>
-                        <div id="bio_<?php echo $list['id'];?>" class="col-md-12 bio" style="height:700px;color:#DCDCDC;"><h1><?php echo $list['dj_name'];?></h1><?php echo html_entity_decode($list['bio'])?></div>					
+				$openedlist=mysqli_fetch_assoc($opened);
+			 	if ($openedlist) { ?>
+                        <div id="bio_<?php echo $openedlist['id'];?>" class="col-md-12 bio" style="height:700px;color:#DCDCDC;"><h1><?php echo $openedlist['dj_name'];?></h1><?php echo html_entity_decode($openedlist['bio'])?></div>					
 		<?php		}else{?>
                         <div id="bioMain" class="col-md-12 bio" style="height:700px;color:#DCDCDC;"><h1>Our Dj's</h1></div>					
 		<?php 		}?>

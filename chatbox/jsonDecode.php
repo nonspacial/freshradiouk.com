0c47398826137665fe342054c7b769993fa360b7
nonspacial <abstrktrobbie@gmail.com>
2014-07-28 03:27:04 +0100

    stuff

--- chatbox/jsonDecode.php
+++ chatbox/jsonDecode.php
@@ -9,21 +9,21 @@ foreach($chat as $chatEcho){
 	$userColour = $dbc->query("SELECT * FROM users WHERE username ='".$chatEcho['chats']['username']."'");
 	$userColour = mysqli_fetch_assoc($userColour);
 	if ($userColour['status'] === 1) {
-		echo "<span style='color:#390;' class='uNameLogin'>" .$chatEcho['chats']['username']. "</span><br>";
-		echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br>";
-		echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br>";
+		echo "<span style='color:#390;' class='uNameLogin'>" .$chatEcho['chats']['username']. "</span><br />";
+		echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br />";
+		echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br />";
 		}elseif($userColour['status'] === 2){
-			echo "<span style='color:#7B0BB4;' class='uNameDJ'>" .$chatEcho['chats']['username']. "</span><br>";
-			echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br>";
-			echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br>";
+			echo "<span style='color:#7B0BB4;' class='uNameDJ'>" .$chatEcho['chats']['username']. "</span><br />";
+			echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br />";
+			echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br />";
 			}elseif($userColour['status'] === 567){
-				echo "<span style='color:#FF0004;' class='uNameAdmin'>" .$chatEcho['chats']['username']. "</span><br>";
-				echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br>";
-				echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br>";
+				echo "<span style='color:#FF0004;' class='uNameAdmin'>" .$chatEcho['chats']['username']. "</span><br />";
+				echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br />";
+				echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br />";
 				}else{
-					echo "<span style='color:#0243CC;' class='uName'>" .$chatEcho['chats']['username']. "</span><br>";
-					echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br>";
-					echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br>";
+					echo "<span style='color:#0243CC;' class='uName'>" .$chatEcho['chats']['username']. "</span><br />";
+					echo "<span class='date'>" . $chatEcho['chats']['time']. "</span><br />";
+					echo "<span class='msg'>". $chatEcho['chats']['message']. "</span><br />";
 					}
 	}
 ?>
